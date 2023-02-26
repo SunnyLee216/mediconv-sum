23576bf80dae2121c64b8c24bec1cd7b59f11842
unknown <757731661@qq.com>
2023-02-26 19:13:19 +0800

    a new alignment

--- baseline_training_pl.py
+++ baseline_training_pl.py
@@ -10,12 +10,15 @@ from transformers import AutoModel, AutoTokenizer
 from model.pegasus import PegasusSummarizer
 import argparse
 import warnings
+import pandas as pd
 warnings.filterwarnings("ignore", message="Error loading punkt: <urlopen error [Errno 111] Connection refused>")
 # Create datasets
 
+import os
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 def main(hparams):
-    # pretrained_model_name = "google/pegasus-summarization"
+    # pretrained_model_name = "google/pegasus-summarization"/"amagzari/pegasus-cnn_dailymail-finetuned-samsum-v2"/
     # tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
     # model = AutoModel.from_pretrained(model_name)
 
@@ -26,6 +29,7 @@ def main(hparams):
     # train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=32,shuffle=True)
 
     # val_dataloader = torch.utils.data.DataLoader(val_dataset,batch_size=32,shuffle=False)
+    # Setting
     tb_logger = TensorBoardLogger(hparams.output_dir, name="lightning_logs")
     checkpoint_callback = ModelCheckpoint(monitor='rouge1_f',
                                           mode='max',
@@ -33,8 +37,10 @@ def main(hparams):
                                           save_top_k=1,
                                           dirpath=os.path.join(tb_logger.log_dir, 'checkpoints'),
                                           filename='ckpt-{epoch:02d}')
-    early_stop_callback = EarlyStopping(monitor='rouge1_f', patience=10,mode="max")
+    early_stop_callback = EarlyStopping(monitor='rouge1_f', patience=6,mode="max")
     lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    # Summarizer
     model = PegasusSummarizer(hparams)
 
     # 监控数值变化 TODO
@@ -43,23 +49,41 @@ def main(hparams):
                       default_root_dir=hparams.output_dir,logger=tb_logger,
                       callbacks=[checkpoint_callback,early_stop_callback,lr_monitor]
                       )
+    # trainer = pl.Trainer(max_epochs=hparams.epochs,
+    #                   devices=None,
+    #                   default_root_dir=hparams.output_dir,logger=tb_logger,
+    #                   callbacks=[checkpoint_callback,early_stop_callback,lr_monitor]
+    #                   )
+
 
     trainer.fit(model)
+    result = trainer.test(ckpt_path="best")
+    
+    df = pd.DataFrame(result[0])
+    result_output_path = os.path.join(hparams.output_dir,"test_results.csv")
+
+    df.to_csv(result_output_path, index=False)
     #trainer.test(dataloaders=dataloader["test"], ckpt_path="best")
 
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_file",type=str, default="./MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-TrainingSet.csv", help="Path to the csv data training file")
     parser.add_argument("--val_file",type=str, default="./MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-ValidationSet.csv", help="Path to the csv data validation file")
-    parser.add_argument("--pretrained_model",type=str, default="google/pegasus-xsum", help="pretrained model and then fine-tune")
+    parser.add_argument("--test_file",type=str, default="./MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-ValidationSet.csv", help="Path to the csv data validation file")
+    parser.add_argument("--pretrained_model",type=str, default="philschmid/bart-large-cnn-samsum", help="pretrained model and then fine-tune")
     parser.add_argument("--output_dir", type=str, default="output", help="Directory to save the outputs")
 
-    parser.add_argument("--max_input_length", type=int, default=2048, help="Max length of the input sequence")
+    parser.add_argument("--max_input_length", type=int, default=1024, help="Max length of the input sequence")
     parser.add_argument("--max_output_length", type=int, default=1024, help="Max length of the output sequence")
-    parser.add_argument("--learning_rate", type=float, default=4e-6, help="Learning rate for the optimizer")
-    parser.add_argument("--batch_size", type=int, default=3, help="batch_size")
+    parser.add_argument("--learning_rate", type=float, default=2e-5, help="Learning rate for the optimizer")
+    parser.add_argument("--batch_size", type=int, default=2, help="batch_size")
     parser.add_argument("--gpus", type=int, default=3, help="gpus")
+    parser.add_argument("--beams", type=int, default=3, help="gpus")
+    parser.add_argument("--warm", type=bool, default=False, help="warm_up")
+    parser.add_argument("--chrf_score", type=bool, default=False, help="chrf_score")
+    parser.add_argument("--rouge_score", type=bool, default=True, help="rouge_score")
+    parser.add_argument("--use_r_drop", type=bool, default=False, help="use_r_drop")
 
-    parser.add_argument("--epochs", type=int, default=300, help="Number of training epochs")
+    parser.add_argument("--epochs", type=int, default=30, help="Number of training epochs")
     hparams = parser.parse_args()
     main(hparams)
--- model/pegasus.py
+++ model/pegasus.py
@@ -1,5 +1,5 @@
 import pytorch_lightning as pl
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM,get_cosine_schedule_with_warmup
 from torchmetrics.text.rouge import ROUGEScore
 from torchmetrics import CHRFScore
 import torch 
@@ -7,58 +7,93 @@ import os
 import torch.nn as nn
 import pandas as pd
 import warnings
+import torch.nn.functional as F
+import csv
 warnings.filterwarnings("ignore", message="Error loading .*: <urlopen error [Errno 111] Connection refused>")
+
+
 class DoctorPatientDialogueDataset(torch.utils.data.Dataset):
-    def __init__(self, csv_file, max_input_length, max_output_length):
+    def __init__(self, csv_file, max_input_length, max_output_length,is_train=True):
         self.data = pd.read_csv(csv_file)
         # self.tokenizer = tokenizer
         self.max_input_length = max_input_length
         self.max_output_length = max_output_length
         self.inputs = self.data['dialogue'].apply(lambda x: x[:self.max_input_length])
-        # TODO 把header_section 的部分拼合早section_text
-        if 'section_text' in self.data.columns:
-            print('TaskA data loading....')
-            self.outputs = self.data.apply(lambda x: x['section_header'] + ' ' + x['section_text'], axis=1)
-            self.outputs = self.outputs.apply(lambda x: x[:self.max_output_length])
-            
+        if self.is_train:
+            if 'section_text' in self.data.columns:
+                print('TaskA data loading....')
+                self.outputs = self.data.apply(lambda x: x['section_header'] + ' ' + x['section_text'], axis=1)
+                self.outputs = self.outputs.apply(lambda x: x[:self.max_output_length])
+                # TODO truncate
 
-        else:
-            print('TaskB or TaskC data loading....')
-            self.outputs = self.data['note'].apply(lambda x: x[:self.max_output_length]) 
-
+            else:
+                print('TaskB or TaskC data loading....')
+                self.outputs = self.data['note'].apply(lambda x: x[:self.max_output_length]) 
+        
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        input_sequence = self.inputs[index]
-        output_sequence = self.outputs[index]
-        return input_sequence, output_sequence
-
-
+        if self.is_train:
+            input_sequence = self.inputs[index]
+            output_sequence = self.outputs[index]
+            return input_sequence, output_sequence
+        else:
+            input_sequence = self.inputs[index]
+            return input_sequence
 
 class PegasusSummarizer(pl.LightningModule):
     def __init__(self, params):
         super().__init__()
         self.params = params
-        self.train_dataset = DoctorPatientDialogueDataset(self.params.train_file, self.params.max_input_length, self.params.max_output_length)
-        self.val_dataset = DoctorPatientDialogueDataset(self.params.val_file, self.params.max_input_length, self.params.max_output_length)
+        self.r_factor = 0.5
+        # PREPARE DATA
+        self.train_dataset = DoctorPatientDialogueDataset(self.params.train_file, self.params.max_input_length, self.params.max_output_length,is_train=True)
+        self.val_dataset = DoctorPatientDialogueDataset(self.params.val_file, self.params.max_input_length, self.params.max_output_length,is_train=True)
+        self.test_dataset = DoctorPatientDialogueDataset(self.params.val_file, self.params.max_input_length, self.params.max_output_length,is_train=False)
+
         # pretrained_model_name = "google/pegasus-summarization"
         pretrained_model_name = self.params.pretrained_model
         self.model = AutoModelForSeq2SeqLM.from_pretrained(pretrained_model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
         self.loss_fn = nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
         self.rouge = ROUGEScore()
-        self.chrf = CHRFScore()
+        if self.params.chrf_score:
+            self.chrf = CHRFScore()
+        
+        self.warm_up=params.warm
         self.learning_rate = params.learning_rate
 
     def forward(self, input_ids, attention_mask, decoder_input_ids):
         outputs = self.model(input_ids, attention_mask=attention_mask,labels=decoder_input_ids,output_hidden_states=True,return_dict=True)
         return outputs.loss,outputs.logits
 
+    def compute_kl_loss(self, p, q, pad_mask=None):
+    
+        p_loss = F.kl_div(F.log_softmax(p, dim=-1), F.softmax(q, dim=-1), reduction='none')
+        q_loss = F.kl_div(F.log_softmax(q, dim=-1), F.softmax(p, dim=-1), reduction='none')
+        
+        # pad_mask is for seq-level tasks
+        if pad_mask is not None:
+            p_loss.masked_fill_(pad_mask, 0.)
+            q_loss.masked_fill_(pad_mask, 0.)
+
+        # You can choose whether to use function "sum" and "mean" depending on your task
+        p_loss = p_loss.sum()
+        q_loss = q_loss.sum()
+
+        loss = (p_loss + q_loss) / 2
+        return loss
+
     def training_step(self, batch, batch_idx):
         input_ids, attention_mask, labels, labels_mask = batch
         loss,logits = self.forward(input_ids, attention_mask,labels)
         # loss = self.loss_fn(logits.view(-1, logits.size(-1)), labels.view(-1))
+        if self.params.use_r_drop:
+            loss2,logits2 = self.forward(input_ids, attention_mask,labels)
+            ce_loss = 0.5*(loss+loss2)
+            kl_loss= self.compute_kl_loss(logits, logits2,pad_mask = labels_mask)
+            loss = ce_loss + self.r_factor*kl_loss
         self.log("train_loss", loss)
         return loss
 
@@ -71,19 +106,23 @@ class PegasusSummarizer(pl.LightningModule):
         # Add the ROUGE evaluation
         # 调整参数使得能够进行多样化的生成
         # TODO 后期把生成的参数放到后面argument
-        outputs = self.model.generate(input_ids, attention_mask=attention_mask,num_beams=5,max_new_tokens = self.params.max_output_length,length_penalty=-1)
-        generated_summaries = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        target_summaries = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
-        
-        rouge_score = self.rouge(generated_summaries, target_summaries)
-        chrf_score = self.chrf(generated_summaries, target_summaries)
-        self.log("chrf_score", chrf_score,sync_dist=True, on_step=False, on_epoch=True,prog_bar=True)
-        self.log("rouge_score", rouge_score,sync_dist=True, on_step=False, on_epoch=True,prog_bar=True)
-        self.log("rougeLsum_f", rouge_score['rougeLsum_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
-        self.log("rouge1_f", rouge_score['rouge1_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
-        self.log("rouge2_f", rouge_score['rouge2_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
+        if self.params.rouge_score:
+            outputs = self.model.generate(input_ids, attention_mask=attention_mask,num_beams=3,max_new_tokens = self.params.max_output_length,length_penalty = 0.5)
+            generated_summaries = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            target_summaries = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
+            
+            rouge_score = self.rouge(generated_summaries, target_summaries)
+            self.log("rouge_score", rouge_score,sync_dist=True, on_step=False, on_epoch=True,prog_bar=True)
+            self.log("rougeLsum_f", rouge_score['rougeLsum_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
+            self.log("rouge1_f", rouge_score['rouge1_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
+            self.log("rouge2_f", rouge_score['rouge2_fmeasure'],sync_dist=True, on_step=False, on_epoch=True)
+        if self.params.chrf_score:
+            chrf_score = self.chrf(generated_summaries, target_summaries)
+            self.log("chrf_score", chrf_score,sync_dist=True, on_step=False, on_epoch=True,prog_bar=True)
 
-        return {'val_loss': loss, "chrf_score":chrf_score, "rougeLsum_fmeasure":rouge_score['rougeLsum_fmeasure'],"rouge1_f":rouge_score['rouge1_fmeasure'],"rouge2_f":rouge_score['rouge2_fmeasure']}
+        
+        
+        # return {'val_loss': loss, "chrf_score":chrf_score, "rougeLsum_fmeasure":rouge_score['rougeLsum_fmeasure'],"rouge1_f":rouge_score['rouge1_fmeasure'],"rouge2_f":rouge_score['rouge2_fmeasure']}
 
     def validation_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
@@ -101,6 +140,32 @@ class PegasusSummarizer(pl.LightningModule):
 
     #     self.train_dataset = TextDataset(X_train, y_train, self.tokenizer, self.params.max_len)
     #     self.val_dataset = TextDataset(X_val, y_val, self.tokenizer, self.params.max_len)
+    def test_step(self, batch, batch_idx):
+        input_ids, attention_mask = batch
+        
+        outputs = self.model.generate(input_ids, attention_mask=attention_mask,num_beams=3,max_new_tokens = self.params.max_output_length,length_penalty = 0.5)
+
+
+        generated_ids = self.model.generate(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        max_length=self.params.max_length,
+        num_beams=self.params.num_beams
+        )
+        preds = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        return {"preds": preds}
+
+    def test_epoch_end(self, outputs):
+        preds = []
+        for output in outputs:
+            preds += output["preds"]
+        return {"preds": preds}
+        # generated_summaries = [summary for output in outputs for summary in output['generated_summaries']]
+        # with open('test.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     writer.writerow(['generated_summary'])
+        #     for summary in generated_summaries:
+        #         writer.writerow([summary])
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.params.batch_size, collate_fn=self.collate_fn, shuffle=True)
@@ -108,13 +173,25 @@ class PegasusSummarizer(pl.LightningModule):
     def val_dataloader(self):
         return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.params.batch_size, collate_fn=self.collate_fn, shuffle=False)
 
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(self.test_dataset, batch_size=1, collate_fn=self.collate_fn_test,shuffle=False)
+    
+
+    def collate_fn_test(self, batch):
+        input_sequences = [x[0] for x in batch]
+        input_ids = self.tokenizer.batch_encode_plus(input_sequences, padding='max_length', return_tensors='pt', max_length=self.params.max_input_length)['input_ids']
+        attention_mask = input_ids.ne(0).long()
+        return input_ids, attention_mask
 
     def collate_fn(self, batch):
+
+        ## 数据增强
         input_sequences = [x[0] for x in batch]
         output_sequences = [x[1] for x in batch]
         # print(input_sequences)
-        input_ids = self.tokenizer.batch_encode_plus(input_sequences, padding=True, return_tensors='pt', max_length=self.params.max_input_length)['input_ids']
-        decoder_input_ids = self.tokenizer.batch_encode_plus(output_sequences, padding=True, return_tensors='pt', max_length=self.params.max_output_length)['input_ids']
+        input_ids = self.tokenizer.batch_encode_plus(input_sequences, padding='max_length', return_tensors='pt', max_length=self.params.max_input_length)['input_ids']
+        decoder_input_ids = self.tokenizer.batch_encode_plus(output_sequences, padding='max_length', return_tensors='pt', max_length=self.params.max_output_length)['input_ids']
+        # print(input_ids)
 
         attention_mask = input_ids.ne(0).long()
         decoder_attention_mask = decoder_input_ids.ne(0).long()
@@ -123,11 +200,17 @@ class PegasusSummarizer(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.learning_rate)
-        lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=self.learning_rate*0.1, max_lr=self.learning_rate, 
-                                                     step_size_up=int(self.params.epochs*0.1), step_size_down=self.params.epochs-int(self.params.epochs*0.1),cycle_momentum=False)
-        scheduler = {'scheduler': lr_scheduler, 'interval': 'step', 'frequency': 1}
-        # return torch.optim.Adam(self.model.parameters(), lr=self.params.learning_rate)
-        return {'optimizer': optimizer,'lr_scheduler': scheduler}
+        if self.warm_up:
+
+            lr_scheduler = get_cosine_schedule_with_warmup(num_warmup_steps=int(0.1*self.params.epochs),num_training_steps=self.params.epochs,)
+            scheduler = {'scheduler': lr_scheduler, 'interval': 'step', 'frequency': 1}
+            get_cosine_schedule_with_warmup
+            return {'optimizer': optimizer,'lr_scheduler': scheduler}
+
+            
+        else:
+            return torch.optim.Adam(self.model.parameters(), lr=self.params.learning_rate)
+        
         
 
     
